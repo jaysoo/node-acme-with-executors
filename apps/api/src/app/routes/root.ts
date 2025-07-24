884090b4c08eb8bdf5ef4410b7b97e0681bea86b
Jack Hsu <jack.hsu@gmail.com>
2025-07-24 09:49:55 -0400

    add util lib

--- apps/api/src/app/routes/root.ts
+++ apps/api/src/app/routes/root.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from 'fastify';
+import { util } from '@node-acme/util';
 
 export default async function(fastify: FastifyInstance) {
   fastify.get('/', async function() {
-    return { message: 'Hello API' };
+    return { message: 'Hello API + ' + util() };
   });
 }
